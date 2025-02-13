970a0f21574803171c6e3393d1ac241b3c717650
Lewis9298 <194249268+Lewis9298@users.noreply.github.com>
2025-02-13 16:57:47 +0000

    completed exercise by use of loops

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(choice) {
+    let newNumber = 1;
+    let oldNumber = 0;
+    if (choice < 0) {
+        return "OOPS"
+    }
+    else {  
+     for (let count = 0; count < choice; count++){
+        let temp = newNumber
+        newNumber = newNumber+oldNumber
+        oldNumber = temp
+     }
+     return oldNumber
+    }
 };
 
 // Do not edit below this line
