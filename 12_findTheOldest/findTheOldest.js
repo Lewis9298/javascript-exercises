adc56fedb9c22713eeb8aa67c4e18e9c85ebce9f
Lewis9298 <194249268+Lewis9298@users.noreply.github.com>
2025-02-13 17:56:04 +0000

    completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.sort((a, b) => {
+    const ageA = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
 
+    return ageB - ageA;
+  })[0];
 };
 
-// Do not edit below this line
 module.exports = findTheOldest;
